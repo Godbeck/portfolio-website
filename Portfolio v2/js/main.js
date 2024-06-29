5235a30884b1f1b250678616df204afe26ad1cc4
Godbeck Odame <59021614+Godbeck@users.noreply.github.com>
2024-06-29 20:40:21 +0000

    Delete Portfolio v2 directory

--- Portfolio v2/js/main.js
+++
@@ -1,8 +0,0 @@
-// MOBILE MENU
-const hamMenu = document.querySelector(".hamburger-button");
-const offScreenMenu = document.querySelector(".mobile-menu");
-
-hamMenu.addEventListener("click", () => {
-  hamMenu.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
